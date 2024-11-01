2bd1552515b62859f29bfb738a73c11d05d44d32
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2024-11-01 13:17:01 +0900

    미들러 Day1 게임 문제 풀이

--- 항해/1-day.py
+++ 항해/1-day.py
@@ -1,17 +1,14 @@
 import math
 
-def winningCount():
-    str = input('게임 횟수, 이긴 횟수 입력: ').split(' ')
-    total = int(str[0])
-    winCount = int(str[1])
-    winning = math.trunc((winCount / total) * 100)
+str = input().split(' ')
+total = int(str[0])
+winCount = int(str[1])
+winning = math.trunc((winCount / total) * 100)
 
-    if total == winCount:
-        return print(-1)
+add = 0
+if total == winCount:
+    add -= 1
 
-    add = 0
-    while winning == (math.trunc(((winCount+add) / (total+add)) * 100)):
-        add += 1
-    return print(add)
-
-winningCount()
+while total != winCount and winning == (math.trunc(((winCount + add) / (total + add)) * 100)):
+    add += 1
+print(add)
