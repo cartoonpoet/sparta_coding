24f0f9824a304287b3bc8172b681b8fc5b625289
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2024-11-11 02:49:48 +0900

    미로 문제풀이구성

---
+++ 3rd_week/3-1.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def maze(grid, start, end):
+
+    return
+
+arr = [
+    [1,1,1,0,1],
+    [1,0,1,0,1],
+    [1,0,1,0,1],
+    [1,1,1,1,1]
+]
+start = [0, 0]
+end = [len(arr)-1, len(arr[0])-1]
+maze(arr, start, end)
