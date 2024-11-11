48cd08754d92258bd549a46ebc881b979fdbde2c
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2024-11-12 00:00:15 +0900

    미로 문제풀이구성

--- 3rd_week/3-1.py
+++ 3rd_week/3-1.py
@@ -1,15 +1,33 @@
 from collections import deque
 
-def maze(grid, start, end):
-
-    return
-
-arr = [
-    [1,1,1,0,1],
-    [1,0,1,0,1],
-    [1,0,1,0,1],
-    [1,1,1,1,1]
+graph = [
+    [1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1]
 ]
-start = [0, 0]
-end = [len(arr)-1, len(arr[0])-1]
-maze(arr, start, end)
+N, M = len(graph), len(graph[0])
+# 상 하 좌 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            # 배열 범위 벗어나는거 감지
+            if (nx < 0) or (ny < 0) or (nx >= N) or (ny >= M):
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    return graph[N-1][M-1]
+
+print(bfs(0, 0))
